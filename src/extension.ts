5fb081d365dc3fda7219c31c51b3e92db9298df8
instantmeboys <instantaneouslydangerous@proton.me>
2023-08-28 11:25:33 -0700

    Integrated most commands into the syntax

--- src/extension.ts
+++ src/extension.ts
@@ -1,5 +1,5 @@
 import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
-    console.log("Activated TagScript");
+    console.log("Activated TagScript TypeScript");
 }
