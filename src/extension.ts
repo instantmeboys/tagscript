33fc90b9fd2b59c9742e1d3ade68d0b91da8d1ca
instantmeboys <instantaneouslydangerous@proton.me>
2023-12-25 20:19:38 -0700

    Made the hover functions more compact

--- src/extension.ts
+++ src/extension.ts
@@ -14,7 +14,7 @@ export function activate(context: vscode.ExtensionContext) {
                 { keyword: "translate", title: "{translate:toTranslate|en}", snippet: "translate:toTranslate|en" },
                 { keyword: "repeat", title: "{repeat:10|text}",snippet: "repeat:10|text" },
                 { keyword: "replace", title: "{replace:toReplace|replacedText|text}", snippet: "replace:toReplace|replacedText|text" },
-                { keyword: "if", title: "{if:test|=|test|then: |else: }", snippet: "if:test|=|test|then: |else: " },
+                { keyword: "if", title: "{if:A|=|A|then: |else: }", snippet: "if:A|=|A|then: |else: " },
                 { keyword: "range", title: "{range:min|max}", snippet: "range:1|10" },
                 { keyword: "set", title: "{set:variable|value}", snippet: "set:variable|value" },
                 { keyword: "get", title: "{get:var}", snippet: "get|var" },
@@ -84,252 +84,66 @@ export function activate(context: vscode.ExtensionContext) {
         }
     }));
 
+    const hoverInfoArray = [
+        { word: "translate", description: "{translate:text|en}", moreInfo: "Translates the text to the language. The language codes are ISO 639-2 or ISO 639-1." },
+        { word: "repeat", description: "{repeat:10|text}", moreInfo: "Repeat text a certain number of times." },
+        { word: "replace", description: "{replace:toReplace|replacedText|text}", moreInfo: "Replaces text." },
+        { word: "args", description : "{args} or {args:1}", moreInfo: "It's the text that goes after the tag name.\nWhen using {args:1}, it will give every arg except {arg:0}. {args:1|-1} will give you all the args with {arg:0} and {arg:-1} sliced off." }, 
+        { word: "id", description : "{id} or {id:user}", moreInfo: "The user id of the person that used the tag, or with {id:} it will try to find the id of someone with that username." }, 
+        { word: "user", description : "{user:input}", moreInfo: "Retrieve your username with {user} or find someone with the name in input." }, 
+        { word: "randmessageid", description : "{randmessageid}", moreInfo: "The random id of one of the past 100 messages." }, 
+        { word: "set", description : "{set:name|value}", moreInfo: "Sets a variable that can be retrieved using {get:}" }, 
+        { word: "get", description : "{get:name}", moreInfo: "Retrieve the value of a variable." }, 
+        { word: "ocr", description : "{ocr} or {ocr:file}", moreInfo: "Reads the text in the latest media sent to a server, or with {ocr:file} it will read that image." }, 
+        { word: "replycontent", description : "{replycontent}", moreInfo: "Retrieves the content that the user replied to." }, 
+        { word: "choose", description : "{choose:A|B|C}", moreInfo: "Choose from a value separated by |" }, 
+        { word: "substring", description : "{substring:text|start|end}", moreInfo: "end is optional.\nRemoves all the text from start to end." }, 
+        { word: "python", description : "{python:code}", moreInfo: "Execute python code" }, 
+        { word: "math", description : "{math:1+1}", moreInfo: "Performs math operations." }, 
+        { word: "code", description : "{code:text}", moreInfo: "Puts the text in a discord formatted codeblock." }, 
+        { word: "jsonify", description : "{jsonify:text}", moreInfo: "Allows new lines in text where it would break code normally, by example in mscript" }, 
+        { word: "messagecontent", description : "{messagecontent:messageid}", moreInfo: "Gets the message content from the message id." }, 
+        { word: "lattach", description : "{lattach}", moreInfo: "Returns the URL of the last attachment in chat." }, 
+        { word: "avatar", description : "{avatar:user}", moreInfo: "Avatar from someone. input is optional, without the input and just {avatar} it returns the avatar of the person using the tag." }, 
+        { word: "argslen", description : "{argslen}", moreInfo: "Gives you the number of arguments" }, 
+        { word: "nick", description : "{nick:user}", moreInfo: "Nickname from someone. input is optional, without the input and just {nick} it returns the nickname of the person using the tag." }, 
+        { word: "attach", description : "{attach:URL}", moreInfo: "Attaches a media URL" }, 
+        { word: "newline", description : "{newline}", moreInfo: "Creates a new line." }, 
+        { word: "text", description : "{text:URL}", moreInfo: "Downloads the source code from an URL." }, 
+        { word: "range", description : "{range:1|10}", moreInfo: "Gets a random number from a value to another in integers." }, 
+        { word: "eval", description : "{eval:text}", moreInfo: "Parses and executes the text, generally useful to use with {text:}" }, 
+        { word: "mention", description : "{mention:user}", moreInfo: "Mentions someone and autocompletes for you." }, 
+        { word: "lower", description : "{lower:TEXT}", moreInfo: "Lowercases all the text." },
+        { word: "upper", description : "{upper:text}", moreInfo: "Uppercases all the text." },
+        { word: "note", description : "{note:text}", moreInfo: "All of the text is treated as a comment, it's never displayed." },
+        { word: "ignore", description : "{ignore:text}", moreInfo: "Ignores script tags and just outputs the raw text." },
+        { word: "arg", description : "{arg:0}", moreInfo: "Gets an argument. 0 is the first, 1 is the second argument and so on." },
+        { word: "reverse", description : "{reverse:text}", moreInfo: "Reverses the text." },
+        { word: "randonline", description : "{randonline}", moreInfo: "Gets a random online user from the server." },
+        { word: "randonlineid", description : "{randonlineid}", moreInfo: "Gets a random online user id from the server." },
+        { word: "len", description : "{len:text}", moreInfo: "Gets the amount of characters in the text." },
+        { word: "length", description : "{length:text}", moreInfo: "Gets the amount of characters in the text." },
+        { word: "", description : "", moreInfo: "" },
+        { word: "", description : "", moreInfo: "" },
+        { word: "", description : "", moreInfo: "" },
+        { word: "", description : "", moreInfo: "" },
 
+      ];
 
     context.subscriptions.push(vscode.languages.registerHoverProvider('tagscript', {
         provideHover(document, position, token) {
             const range = document.getWordRangeAtPosition(position);
             const word = document.getText(range);
 
-            if (word === 'replace') {
-                return {
-                    contents: [
-                        '{replace:with|to|in}',
-                        'The text that matches **with** in **in** will be replaced to **to**, you can nest replace functions.'
-                    ]
-                };
-            } else if (word === 'args') {
-                return {
-                    contents: [
-                        '{args}',
-                        'The text that goes after the tag name.'
-                    ]
-                };
-            } else if (word === 'id') {
-                return {
-                    contents: [
-                        '{id} or {id:username}',
-                        'The user id of the person that used the tag, or with {id:} it will try to find the id of someone with that username.'
-                    ]
-                };
-            } else if (word === 'user') {
-                return {
-                    contents: [
-                        '{user:input}',
-                        'Retrieve your username with {user} or find someone with the name in input.'
-                    ]
-                };
-            } else if (word === 'randmessageid') {
-                return {
-                    contents: [
-                        '{randmessageid}',
-                        'The random id of one of the past 100 messages.'
-                    ]
-                };
-            } else if (word === 'set') {
-                return {
-                    contents: [
-                        '{set:animal|cats}',
-                        'Sets a variable that can be retrieved using {get:animal}'
-                    ]
-                };
-            } else if (word === 'get') {
-                return {
-                    contents: [
-                        '{get:variable}',
-                        'Retrieves the value of a variable set using {set:variable|content}'
-                    ]
-                };
-            } else if (word === 'ocr') {
-                return {
-                    contents: [
-                        '{ocr} or {ocr:file}',
-                        'Reads the text in the latest media sent to a server, or with {ocr:file} it will read that image.'
-                    ]
-                };
-            } else if (word === 'translate') {
-                return {
-                    contents: [
-                        '{translate:text|en}',
-                        'Translates the text to the language. The language codes are ISO 639-2 or ISO 639-1.'
-                    ]
-                };
-            } else if (word === 'replycontent') {
-                return {
-                    contents: [
-                        '{replycontent}',
-                        'Retrieves the content that the user replied to.'
-                    ]
-                };
-            } else if (word === 'choose') {
-                return {
-                    contents: [
-                        '{choose:cba|abc|bca}',
-                        'Picks one of the random items separated by |'
-                    ]
-                };
-            } else if (word === 'substring') {
-                return {
-                    contents: [
-                        '{substring:text|start|end}',
-                        'end is optional.',
-                        'Removes all the text from start to end'
-                    ]
-                };
-            } else if (word === 'python') {
-                return {
-                    contents: [
-                        '{python:}',
-                        'Executes python code.',
-                    ]
-                };
-            } else if (word === 'math') {
-                return {
-                    contents: [
-                        '{math:1 + 1}',
-                        'Returns the result of the operation.',
-                    ]
-                };
-            } else if (word === 'code') {
-                return {
-                    contents: [
-                        '{code:hello}',
-                        'Puts the text in a codeblock.',
-                    ]
-                };
-            } else if (word === 'jsonify') {
-                return {
-                    contents: [
-                        '{jsonify:text}',
-                        'Allows new lines in text where it would break code normally, by example in mscript',
-                    ]
-                };
-            } else if (word === 'messagecontent') {
-                return {
-                    contents: [
-                        '{messagecontent:messageid}',
-                        'Gets the message content from the message id.',
-                    ]
-                };
-            } else if (word === 'lattach') {
-                return {
-                    contents: [
-                        '{lattach}',
-                        'Returns the URL of the last attachment in chat.',
-                    ]
-                };
-            } else if (word === 'argslen') {
-                return {
-                    contents: [
-                        '{argslen}',
-                        'Returns the amount of arguments.',
-                    ]
-                };
-            } else if (word === 'avatar') {
-                return {
-                    contents: [
-                        '{avatar:input}',
-                        'Avatar from someone. input is optional, without the input and just {avatar} it returns the avatar of the person using the tag.',
-                    ]
-                };
-            } else if (word === 'nick') {
-                return {
-                    contents: [
-                        '{nick:input}',
-                        'NIckname from someone. input is optional, without the input and just {nick} it returns the nickname of the person using the tag.',
-                    ]
-                };
-            } else if (word === 'attach') {
-                return {
-                    contents: [
-                        '{attach:URL}',
-                        'Attaches a media URL.',
-                    ]
-                };
-            } else if (word === 'newline') {
-                return {
-                    contents: [
-                        '{newline}',
-                        'Creates a new line.',
-                    ]
-                };
-            } else if (word === 'text') {
-                return {
-                    contents: [
-                        '{text:URL}',
-                        'Downloads the source code from an URL',
-                    ]
-                };
-            } else if (word === 'range') {
-                return {
-                    contents: [
-                        '{range:1|10}',
-                        'Gets a random number from 1 to 10.',
-                    ]
-                };
-            } else if (word === 'lower') {
-                return {
-                    contents: [
-                        '{lower:text}',
-                        'Makes the text all lowercase.',
-                    ]
-                };
-            } else if (word === 'upper') {
-                return {
-                    contents: [
-                        '{upper:text}',
-                        'Makes the text all uppercase.',
-                    ]
-                };
-            } else if (word === 'length') {
-                return {
-                    contents: [
-                        '{length:text}',
-                        'Gets the amount of characters in text.',
-                    ]
-                };
-            } else if (word === 'len') {
-                return {
-                    contents: [
-                        '{len:text}',
-                        'Gets the amount of characters in text.',
-                    ]
-                };
-            } else if (word === 'note') {
-                return {
-                    contents: [
-                        '{note:text}',
-                        'All of the text is treated as a comment and never displayed.',
-                    ]
-                };
-            } else if (word === 'ignore') {
-                return {
-                    contents: [
-                        '{ignore:text}',
-                        'Ignores script tags in text.',
-                    ]
-                };
-            } else if (word === 'eval') {
-                return {
-                    contents: [
-                        '{eval:text}',
-                        'Parses and runs the text.',
-                    ]
-                };
-            } else if (word === 'randonline') {
-                return {
-                    contents: [
-                        '{randonline}',
-                        'Gets a random online user from the server.',
-                    ]
-                };
-            } else if (word === 'randonlineid') {
-                return {
-                    contents: [
-                        '{randonlineid}',
-                        'Gets a random id from an online user in the server.',
-                    ]
-                };
+            const hoverInfo = hoverInfoArray.find(entry => entry.word === word);
+
+            if (hoverInfo) {
+                const hoverRange = document.getWordRangeAtPosition(position);
+                const hoverText = new vscode.MarkdownString();
+
+                hoverText.appendMarkdown(`**${hoverInfo.description}**\n\n${hoverInfo.moreInfo}`);
+
+                return new vscode.Hover(hoverText, hoverRange);
             }
 
             return null;
